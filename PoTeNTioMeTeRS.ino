0472fa4c9c26541681592d9c7ecab24d427d57e5
BurYE <student@ent-v321-010.local>
2017-10-10 17:41:24 -0400

    meh

---
+++ PoTeNTioMeTeRS.ino
@@ -0,0 +1,19 @@
+int potPin = 4;
+int speakerPin = 7;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(speakerPin, OUTPUT);
+}
+
+void loop() {
+  
+int val = analogRead(potPin);
+int mappedVal = map(val,0,990,50,2);
+Serial.println(mappedVal);
+
+digitalWrite(speakerPin, HIGH);
+delay(mappedVal);
+digitalWrite(speakerPin,LOW);
+delay(mappedVal);
+}
